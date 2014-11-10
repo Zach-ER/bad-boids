99d3aa4cfd84f0cd875899cec1f769e5a34bd780
zer <zach.earos@gmail.com>
2014-11-10 18:02:50 +0000

    wrote test for dont crash

--- test_boids.py
+++ test_boids.py
@@ -2,6 +2,7 @@ from boids import update_boids
 from nose.tools import assert_almost_equal
 import os
 import yaml
+import boids as b
 
 def test_bad_boids_regression():
     regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
@@ -11,3 +12,7 @@ def test_bad_boids_regression():
         for after_value,before_value in zip(after,before): 
             assert_almost_equal(after_value,before_value,delta=0.01)
 	
+def test_dont_crash():
+    xvs,yvs,xs,ys  = [[0,1],[2,3],[0,0],[1,2]]
+    xs,ys= b.dont_crash(xs,ys,xvs,yvs)
+    assert xs == [0,1] and ys == [1,4]
